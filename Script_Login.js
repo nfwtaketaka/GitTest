ece50cf698cb07efdf2eec7021c01400cc2bf64e
taketaka <nfw.taketaka@gmail.com>
2024-01-27 23:40:08 +0900

    test

---
+++ Script_Login.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function RironCsharp(event) {
+
+    //Še—v‘f‚ğæ“¾
+    let area = document.getElementById("area");
+    let input = area.querySelector("input[name=input]");
+    let pass = area.querySelector("input[name=pass]");
+
+    //user name ‚Æpass‚ğ”FØ
+    if (input.value == "nfwdekitaka" && pass.value == "nfw4103") {
+        //location.href = "D:/10_VisulalStudio/14_HTML/06_Mokuji/CSharpGrammerMokuji/CSharpGrammerMokuji/HTMLPage_CSharpGrammerMokuji.html";
+        location.href = "HTMLPage_Command.html";
+    }
+}
